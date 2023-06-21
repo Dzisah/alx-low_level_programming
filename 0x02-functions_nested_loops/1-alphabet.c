692e421b3c52c35a46b0caf15b2f7d049395e708
Dzisah <Dzisah>
2023-06-20 20:42:40 -0700

    fucntions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+  2 
+  3 /**
+  4 * print_alphabet - Make the alphabet
+  5 *
+  6 * Return: void
+  7 */
+  8 void print_alphabet(void)
+  9 {       
+ 10 char ch;
+ for (ch = 'a'; ch <= 'z'; ch++;)
+_putchar(ch);
+_putchar('\n');
+  }
